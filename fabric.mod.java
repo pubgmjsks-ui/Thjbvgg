4f43a87e9db42059007777f6b275aeca557d992d
pubgmjsks-ui <pubgmjsks@gmail.com>
2026-05-06 05:59:23 +0300

    Add files via upload

---
+++ fabric.mod.java
@@ -0,0 +1,13 @@
+{
+  "schemaVersion": 1,
+  "id": "yakushaclient",
+  "version": "1.0",
+  "name": "Yakusha Client",
+  "mixins": [],
+  "entrypoints": {
+    "client": ["com.yakusha.client.YakushaMod"]
+  },
+  "depends": {
+    "fabric": "*"
+  }
+}
