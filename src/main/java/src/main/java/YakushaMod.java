aa48244ad92c236285ed500cf3d0baddb02e3732
pubgmjsks-ui <pubgmjsks@gmail.com>
2026-05-06 20:51:04 +0300

    Delete src/main/java/src/main/java directory

--- src/main/java/src/main/java/YakushaMod.java
+++
@@ -1,9 +0,0 @@
-package net.yakusha.mod;
-import net.fabricmc.api.ModInitializer;
-
-public class Main implements ModInitializer {
-    @Override
-    public void onInitialize() {
-        System.out.println("Yakusha Mod Loaded!");
-    }
-}
