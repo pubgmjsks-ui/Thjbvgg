f2fc06e111217d6afcfce499971e7b93107034a5
pubgmjsks-ui <pubgmjsks@gmail.com>
2026-05-06 20:32:16 +0300

    Update fabric.mod.java

--- fabric.mod.java
+++ fabric.mod.java
@@ -3,13 +3,20 @@
   "id": "yakushamod",
   "version": "1.0.0",
   "name": "Yakusha Mod",
+  "description": "Powerful cheat client",
+  "authors": [ "Yakusha" ],
+  "contact": { "sources": "https://github.com" },
+  "license": "MIT",
+  "icon": "assets/yakushamod/icon.png",
+  "environment": "client",
   "entrypoints": {
-    "main": [
-      "net.yakusha.mod.Main"
+    "client": [
+      "com.yakusha.client.YakushaMod"
     ]
   },
   "depends": {
     "fabricloader": ">=0.11.3",
+    "fabric": "*",
     "minecraft": "1.16.5"
   }
 }
